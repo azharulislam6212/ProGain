97b57886ee49756ea39bab235288723875d58bd0
Azharul Islam <124974120+azharulislam6212@users.noreply.github.com>
2026-05-03 18:42:58 +0600

    updated icon

--- assets/components.js
+++ assets/components.js
@@ -1,28 +1,26 @@
 export function initButtons(root = document) {
   root.querySelectorAll(".btn").forEach(btn => {
     if (btn.classList.contains("is-ready")) return;
-
-    if (btn.classList.contains("btn--icon-only") || btn.classList.contains("btn--solid-icon")  || btn.classList.contains("btn--default-icon") || btn.classList.contains("btn--icon-none")) {
+    const hasIconVariant = [...btn.classList].some(c => c.includes("btn--icon"));
+    if (hasIconVariant) {
       const el = btn.querySelector(".btn__content");
       const textEl = el?.querySelector(".btn__text");
       if (!el) return;
-
       const iconEl = el.querySelector(".btn__icon");
+      const iconClasses = iconEl ? [...iconEl.classList].filter(c => c !== "btn__icon").join(" ") : "";
       const icon = iconEl?.querySelector("svg")?.outerHTML || "";
-
       btn.insertAdjacentHTML("beforeend", `
         <span class="btn__hover">
           <span class="btn__hover-circle">
             ${textEl ? `<span class="btn__hover-text">${textEl.textContent}</span>` : ""}
             ${icon ? `<span class="btn__hover-icons">
-              <span class="btn__hover-icon">${icon}</span>
-              <span class="btn__hover-icon">${icon}</span>
+              <span class="btn__hover-icon ${iconClasses}">${icon}</span>
+              <span class="btn__hover-icon ${iconClasses}">${icon}</span>
             </span>` : ""}
           </span>
         </span>
       `);
     }
-
     btn.classList.add("is-ready");
   });
 }
